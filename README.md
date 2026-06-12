5252d3a1fd2c5d987a414772f2caf010964b4380
Yuu Akaoka <c0a25004a6@edu.teu.ac.jp>
2026-06-12 16:39:43 +0900

    ehettenandayo

---
+++ README.md
@@ -0,0 +1,96 @@
+# Turtlebot3 Lime application sample with Behavior Tree and ros_actor
+This repository contains demo code for Lime, the next generation TurtleBot3 with a Jetson GPU and a 6-DOF manipulator, running in a single PC environment using Docker.
+
+## Installation
+- Go to working directory of any location
+- Make sure the DISPLAY environment variable is set properly
+
+Misconfiguration of X windows can be a source of trouble. The X server must be configured to accept access via tcpip (e.g. xhost +).  
+Value of the DISPLAY environment variable should be:  
+'IP address of host os':0.0  
+
+```
+$ printenv DISPLAY # for checking DISPLAY environment variable
+$ git pull https://github.com/momoiorg-repository/LimeSimulDemo.git
+$ cd LimeSimulDemo
+$ docker compose build
+$ docker compose up
+```
+
+By these operations, docker will now start and an X window will appear. From now on, we will work within this window.  The first thing you need to do is register the package you'll be running here. The package sources are in ~/pytwb_ws, and the following operations register the 'cm1' package there, which contains various sample codes you can run.
+```
+# cd ~/pytwb_ws
+# pytwb
+> create cm1
+Y
+```
+
+The installation is now complete.
+
+## Demo Code Execution
+
+
+https://github.com/user-attachments/assets/d8400567-b3ed-41d0-9b63-0da5fb4651f3
+
+
+- Launching the simulation environment
+
+```
+# run_all
+```
+
+You will now see one Gazebo screen and two Rviz screens.
+
+As a next step, run sample codes which are in Behavior Tree or ros_actor actor format. They can be executed from the command interpreter provided by ros_actor. The source code for these is located under ~/pytwb_ws/src/cm1.
+
+- Launch vscode, attach to 'limesimuldemo' docker from Remote Explorer, and open the /root/pytwb_ws directory.
+
+- When you run '~/pytwb_ws/src/cm1/\_\_main\_\_.py from vscode, a command prompt will appear.
+
+- From the command prompt, type 'bt_search' to start the demo.  The operation executes the 'bt_search' Behavior Tree.
+
+To run the demo, you need to add Coke Can
+from Gazebo's Insert tab.  Any Coke Can position should work.
+
+##  Grasping Coke Can
+You can also try to get a robot to grasp Coke Cans by running 'bt_catch' Behavior Tree.
+The original Coke Can model is too heavy to pick as it is, so edit the model and set the weight to around 0.04kg.
+- Place the Coke Can in any position, select it with the mouse and select 'Edit model' from the operation list displayed by right-clicking.
+- The entire screen will turn white, so select the Coke Can again and double left-click to display the editing screen.
+- From there, change the Mass value from 0.39 kg to 0.039 kg and click 'ok'
+
+![Edit Coke Can properties](doc/weight.png)
+
+- Exit model editing mode with cntl-X and save the edited model with a different name
+
+- Place the reduced weight Coke Can in a position directly visible to the robot and start 'bt_catch'.
+
+## Picking Operation
+The picking operation, which combines bt_search and bt_catch, can be executed by the 'bt_pick' Behavior Tree.
+
+## Internal structure
+ros_actor is heavily used to implement each Behavior Tree. As the main thing,
+- Nav2 operation,
+- MoveIt2 operation,
+- Analysis of camera information,
+
+etc.
+
+[Detailed description](doc/app.pdf)
+
+## ros_actor mechanism documentation
+- [Principle of ros_actor](doc/principle.pdf)
+
+## Related repositories and sites
+- [ros_actor](https://github.com/momoiorg-repository/ros_actor)
+- [pytwb](https://github.com/momoiorg-repository/pytwb)
+- [vector_map](https://github.com/RobotSpatialCognition/vector)
+
+## Dependencies  
+Special thanks to the following works:  
+- [“turtlebot3_behavior_demos”](https://github.com/sea-bass/turtlebot3_behavior_demos) by sea-bass  
+Gazebo world is derived from this repository.
+- [lecture page of OpenCV and Python](https://demura.net/education/22777.html) by Demura Kosei  
+
+## About us
+[momoi.org](https://momoi.org/?yada_wiki=ros-related-projects)
---
+++ pytwb_ws/src/cm1/cm1/lib/actor/tools.py
@@ -0,0 +1,280 @@
+from math import atan2, degrees, radians, hypot
+import os
+
+from pyquaternion import Quaternion
+
+from pytwb import lib_main
+from ros_actor import SubNet, actor, register_bt
+from ..pointlib import PointEx
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+import subprocess
+import re
+
+class Tools(SubNet):
+    # command version
+    @actor
+    def go(self, *arg):
+        arg = list(map(float, arg))
+        while len(arg) < 3:
+            arg.append(0.0)
+        return self.run_actor('goto', arg[0], arg[1], arg[2])
+
+    @actor
+    def update_bt(self):
+        package = lib_main.get_package()
+        dir = os.path.join(package.path, 'trees')
+        for d in os.listdir(dir):
+            if not d.endswith('.xml'): continue
+            name = d[:-4]
+            register_bt(name)
+        return True
+    
+    # show gripper pose
+    @actor
+    def gl(self):
+        trans = self.run_actor("gripper_trans")
+        trans = trans.transform
+        offset = trans.translation
+        rot = trans.rotation
+        point = (0.0, 0.0, 0.0)
+        q_rot = Quaternion(rot.w, rot.x, rot.y, rot.z)
+        rot_point = q_rot.rotate(point)
+        x = rot_point[0] + offset.x
+        y = rot_point[1] + offset.y
+        print(f'gripper angle:{degrees(atan2(y, x))}')
+    
+    @actor
+    def forward(self, value):
+        self.run_actor('adjust_joint', 0.0, value, 0.0, 0.0)
+    
+    @actor
+    def ol(self):
+        _,_,target_angle,distance = self.run_actor('measure_center', target='base_link', assumed=0.2)
+        print(f'target_angle:{degrees(target_angle)}, distance:{distance}')
+    
+    @actor
+    def tl(self):
+        _, _, angle = self.run_actor('object_loc', 'base_link')
+        point = self.run_actor('find_object')
+        print(f'object angle:{degrees(angle)}, distance:{point.distance}') 
+    
+    @actor    
+    def js(self):
+        value = self.get_value('joint_stat')
+        d_value = tuple(map(degrees, value))
+        print(d_value)
+    
+    @actor
+    def cpos(self):
+        root = PointEx()
+        ref = PointEx(1.0, 0.0)
+        trans = self.run_actor('map_trans')
+        if not trans: return
+        root.setTransform(trans.transform)
+        ref.setTransform(trans.transform)
+        print(f'x:{root.x}, y:{root.y}, z:{degrees(root.z)}')
+        rx = ref.x - root.x
+        ry = ref.y - root.y
+        robot_angle = atan2(ry, rx)
+        print(f'robot angle to X axis: {degrees(robot_angle)}')
+    
+    @actor
+    def pause(self, is_on=True):
+        if is_on: input('debug pause')
+        return True
+    
+    @actor
+    def key(self):
+        return input()
+
+    @actor
+    def angle(self):
+        print(f'assumed:{degrees(atan2(0.5, 1.0))}')
+        x, y, angle = self.run_actor('object_loc')
+        print(f'angle:{degrees(angle)}')
+
+    @actor
+    def gripper_angle(self, angle):
+        gripper = self.get_value('gripper')
+        gripper.move_to_position(angle)
+        self.run_actor('sleep', 2)
+        return True
+    
+    @actor
+    def get_gripper(self):
+        joints = self.run_actor('joints')
+        target_joint = 'gripper_left_joint'
+        idx = joints.name.index(target_joint)
+        pos = round(joints.position[idx], 2)
+        return pos
+    
+    @actor
+    def get_arm_angle(self):
+        joints = self.run_actor('joints')
+        target_joint = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+        angle_list = []
+        for i in target_joint:
+            idx = joints.name.index(i)
+            pos = round(degrees(joints.position[idx]), 1)
+            angle_list.append(pos)
+        return angle_list
+
+    @actor 
+    def shot(self, fpath):
+        cv_image = self.run_actor('pic_receiver')
+        pt = fpath + ".png"
+        cv2.imwrite(pt, cv_image)
+    
+    @actor
+    def depth_shot(self, fpath):
+        data = self.run_actor('depth')
+        cv_bridge = self.get_value('cv_bridge')
+        depth_image = cv_bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+        # 適切な画像になるように正規化している
+        normalized_depth = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX)
+        normalized_depth = np.uint8(normalized_depth)
+
+        pt = fpath + ".png"
+        cv2.imwrite(pt, normalized_depth)
+
+    def pix_to_coordinate(self, x, y, distance):
+        intrinsics = self.get_value('intrinsics')
+        p = rs.rs2_deproject_pixel_to_point(intrinsics,[x,y], distance)
+        return p[2],-p[0]
+
+    @actor
+    def go_front(self):
+        x, y, theta = self.run_actor("object_front", "map")
+        self.run_actor("goto", x, y, theta)
+
+    @actor
+    def attach(self):
+        obj = self.run_actor("choose_pick_obj")
+        if obj is None:
+            print("No object. Aborted")
+            return False
+        
+        link = self.run_actor("get_linkname", obj)
+
+        cmd = f"""ros2 service call /ATTACHLINK linkattacher_msgs/srv/AttachLink "{{model1_name: 'turtlebot3_lime_system', link1_name: 'link7', model2_name: '{obj}', link2_name: '{link}'}}" """
+        subprocess.run(cmd, shell=True)
+        return True
+    
+    @actor
+    def detach(self):
+        obj = self.run_actor("choose_pick_obj")
+        if obj is None:
+            print("No object. ")
+            return False
+        link = self.run_actor("get_linkname", obj)
+
+        cmd = f"""ros2 service call /DETACHLINK linkattacher_msgs/srv/DetachLink "{{model1_name: 'turtlebot3_lime_system', link1_name: 'link7', model2_name: '{obj}', link2_name: '{link}'}}" """
+        subprocess.run(cmd, shell=True)
+        return True
+
+    @actor
+    def get_link7(self):
+        link_info = self.run_actor("link_states")
+        names = link_info.name
+        index = names.index("turtlebot3_lime_system::link7")
+
+        cod = link_info.pose[index].position
+        return (cod.x, cod.y)
+    
+    @actor
+    def get_end_effector(self):
+        link_info = self.run_actor("link_states")
+        names = link_info.name
+
+        right = names.index("turtlebot3_lime_system::gripper_right_link")
+        left = names.index("turtlebot3_lime_system::gripper_left_link")
+
+        r_cod = link_info.pose[right].position
+        l_cod = link_info.pose[left].position
+        return r_cod.x, r_cod.y, l_cod.x, l_cod.y
+
+    @actor
+    def get_model_list(self):
+        model_info = self.run_actor("model_states")
+        return model_info.name
+
+    @actor
+    def get_linkname(self, model_name):
+        link_info = self.run_actor("link_states")
+        names = link_info.name
+        target = next((s for s in names if model_name in s), None)
+        match = re.search(rf"^{model_name}::(.+)$", target)
+        if match:
+            link_name = match.group(1)
+        return link_name
+        
+    @actor 
+    def get_modelpos_dict(self, debug:bool = False):
+        pos_dict = {}
+        model_info = self.run_actor("model_states")
+        for i in range(len(model_info.name)):
+            if i < 3:
+                if debug:
+                    print(model_info.name[i])
+            else:
+                cod = model_info.pose[i].position
+                pos_dict[model_info.name[i]] = (cod.x, cod.y)
+        return pos_dict
+    
+    @actor 
+    def get_linkpos_dict(self):
+        pos_dict = {}
+        link_info = self.run_actor("link_states")
+        for i in range(len(link_info.name)):
+            cod = link_info.pose[i].position
+            pos_dict[link_info.name[i]] = (cod.x, cod.y)
+        return pos_dict
+
+    @actor
+    # def get_near_obj(self, debug:bool =False):
+    def choose_pick_obj(self, debug:bool =False):
+        gripper = self.run_actor("get_link7")
+        model_dict = self.run_actor("get_modelpos_dict")
+        name_list = []
+
+        for objname in model_dict:
+            dist = hypot(gripper[0]-model_dict[objname][0], gripper[1]-model_dict[objname][1])
+            if debug:
+                print(f"{objname}: {dist}")
+
+            if dist <= 0.14:
+                name_list.append(objname)
+                return objname
+        return None
+        # if len(name_list) == 0:
+        #     return False
+        # return name_list
+
+
+    # @actor 
+    # def choose_pick_obj(self):
+    #     name_list = self.run_actor("get_near_obj")
+    #     r_x, r_y, l_x, l_y = self.run_actor("get_end_effector")
+    #     model_dict = self.run_actor("get_modelpos_dict")
+
+    #     if not name_list:
+    #         return False
+        
+    #     for n in name_list:
+    #         print(model_dict[n])
+
+    #     objname = None
+    #     return objname
+
+    @actor
+    def make_symbolic_link(self):
+        dir_path = "/root/practice_ws/trees"
+        org_path = "/root/pytwb_ws/src/cm1/cm1/trees"
+        for d in os.listdir(dir_path):
+            if not d.endswith('.xml'): continue
+            if d not in os.listdir(org_path):
+                cmd = f"""ln -s /root/practice_ws/trees/{d} /root/pytwb_ws/src/cm1/cm1/trees/{d}"""
+                subprocess.run(cmd, shell=True)
+        return True
