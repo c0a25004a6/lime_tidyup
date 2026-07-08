3bc1e426b166f35def8ac331dc5224c99dbc9ae1
Yuu Akaoka <c0a25004a6@edu.teu.ac.jp>
2026-07-08 18:31:34 +0900

    model large

--- pytwb_ws/src/cm1/cm1/lib/actor/tools.py
+++ pytwb_ws/src/cm1/cm1/lib/actor/tools.py
@@ -150,7 +150,7 @@ class Tools(SubNet):
 
     @actor
     def voice_recognize(
-        self, language="ja", key="recognized_text", device_index=-1, model="base"
+        self, language="ja", key="recognized_text", device_index=-1, model="large"
     ):
         import tempfile
 
