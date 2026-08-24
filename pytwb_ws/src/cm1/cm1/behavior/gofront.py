b7e9b0be53719ffd1fb08400deb7b544fa2ef2d1
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-08-24 16:51:37 +0900

    簡易3D姿勢推定を追加

--- pytwb_ws/src/cm1/cm1/behavior/gofront.py
+++ pytwb_ws/src/cm1/cm1/behavior/gofront.py
@@ -4,7 +4,7 @@ from lib.actor_bt import ActorBT
 
 @behavior
 class GoFrontCube(ActorBT):
-    desc = 'turn toward cube and approach using depth'
+    desc = 'turn toward cube and approach using accepted 3D pose or depth'
 
     def __init__(
         self,
@@ -14,7 +14,8 @@ class GoFrontCube(ActorBT):
         stop_distance=0.30,
         forward_speed=0.03,
         turn_speed=0.20,
-        target_offset_px=40.0
+        target_offset_px=40.0,
+        pose_max_reprojection_error_px=4.0
     ):
         super(GoFrontCube, self).__init__(
             name,
@@ -26,6 +27,9 @@ class GoFrontCube(ActorBT):
         self.forward_speed = float(forward_speed)
         self.turn_speed = float(turn_speed)
         self.target_offset_px = float(target_offset_px)
+        self.pose_max_reprojection_error_px = float(
+            pose_max_reprojection_error_px
+        )
 
     def initialise(self):
         super().prepare()
@@ -36,7 +40,9 @@ class GoFrontCube(ActorBT):
             f'stop_distance={self.stop_distance}',
             f'forward_speed={self.forward_speed}',
             f'turn_speed={self.turn_speed}',
-            f'target_offset_px={self.target_offset_px}'
+            f'target_offset_px={self.target_offset_px}',
+            'pose_max_reprojection_error_px='
+            f'{self.pose_max_reprojection_error_px}'
         )
 
         self.shared.set_callee([
@@ -47,7 +53,8 @@ class GoFrontCube(ActorBT):
                     self.stop_distance,
                     self.forward_speed,
                     self.turn_speed,
-                    self.target_offset_px
+                    self.target_offset_px,
+                    self.pose_max_reprojection_error_px
                 )
             )
         ])
