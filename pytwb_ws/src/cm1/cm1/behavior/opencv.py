91e1cc561752bd90275d2c3d2eb40d94c10641bc
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-08-25 11:39:22 +0000

    dev2 Yuu identity normalization receipt v2

--- pytwb_ws/src/cm1/cm1/behavior/opencv.py
+++
@@ -1,9 +0,0 @@
-import py_trees
-from lib.actor_bt import ActorBT
-from pytwb.common import behavior
-
-
-@behavior
-class QRScan(ActorBT):
-    def __init__(self, name, node):
-        super().__init__(name, "qr_scan")
--- pytwb_ws/src/python/python/app.py
+++
@@ -1,5 +0,0 @@
-
-from pytwb.lib_main import run
-def app_main(trees):
-# insert application specific initialization routine here
-    run(trees)
